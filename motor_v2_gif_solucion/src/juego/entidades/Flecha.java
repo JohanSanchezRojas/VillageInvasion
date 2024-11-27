485666b1fdc502bb378198c9077ec0cde64e3e21
JohanSanchezRojas <118130981+JohanSanchezRojas@users.noreply.github.com>
2024-11-27 14:57:23 -0600

    Update Flecha.java

--- motor_v2_gif_solucion/src/juego/entidades/Flecha.java
+++ motor_v2_gif_solucion/src/juego/entidades/Flecha.java
@@ -1,5 +1,6 @@
 package juego.entidades;
 
+import motor_v1.motor.component.Collider;
 import motor_v1.motor.entidades.SpriteMovible;
 import motor_v1.motor.util.Vector2D;
 import java.awt.Graphics;
@@ -7,16 +8,10 @@ import java.awt.image.BufferedImage;
 
 import juego.Assets;
 
-public class Flecha extends Proyectil {
+public class Flecha extends SpriteMovible{
 
-	private double velocidad = 10;
-	
-	public Flecha(String nombre, int dano, Vector2D posicionInicial, double angulo, SpriteMovible sprite) {
-		super(nombre, dano, posicionInicial, angulo, sprite);
-		// TODO Auto-generated constructor stub
+	public Flecha(String nombre, BufferedImage textura, Vector2D posicion) {
+		super(nombre, textura, posicion);
 	}
-
 	
-
-  
 }
