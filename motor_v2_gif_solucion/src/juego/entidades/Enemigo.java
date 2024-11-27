b65cec50afb83b204ba7014ae34dd64824bc2b8d
AndrewMM20 <andrew2012mm@gmail.com>
2024-11-27 15:09:02 -0600

    Update Enemigo.java

--- motor_v2_gif_solucion/src/juego/entidades/Enemigo.java
+++ motor_v2_gif_solucion/src/juego/entidades/Enemigo.java
@@ -5,15 +5,16 @@ import java.awt.image.BufferedImage;
 
 import juego.Assets;
 import motor_v1.motor.Entidad;
+import motor_v1.motor.component.Collider;
 import motor_v1.motor.entidades.SpriteMovible;
 import motor_v1.motor.util.Vector2D;
 
 public abstract class Enemigo extends Entidad {
 
 	public SpriteMovible cuerpo;
-	protected int Movimiento;
+	protected int movimientoE;
 	private int numeroVidas;
-	
+	protected int crono;
 	
 	public Enemigo(String nombre, BufferedImage textura, Vector2D posicion) {
 		super();
@@ -22,10 +23,14 @@ public abstract class Enemigo extends Entidad {
 
 	
 	public void actualizar () {
-		
+		cuerpo.getColisiona().actualizar();
 	}
 	
 	
 	public abstract void MovimientoEnemigo();
+	
+	public Collider getColisiona() {
+		return cuerpo.getColisiona();
+	}
 }
 //{}
