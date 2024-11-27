b21310cf9e0eaf548742a7a9e794ce027e1b27b7
AndrewMM20 <andrew2012mm@gmail.com>
2024-11-26 20:50:14 -0600

    Update Enemigo.java

--- motor_v2_gif_solucion/src/juego/entidades/Enemigo.java
+++ motor_v2_gif_solucion/src/juego/entidades/Enemigo.java
@@ -1,17 +1,23 @@
 package juego.entidades;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 
+import juego.Assets;
+import motor_v1.motor.Entidad;
 import motor_v1.motor.entidades.SpriteMovible;
 import motor_v1.motor.util.Vector2D;
 
-public abstract class Enemigo extends SpriteMovible {
+public abstract class Enemigo extends Entidad {
 
-	
+	public SpriteMovible cuerpo;
 	protected int Movimiento;
+	private int numeroVidas;
+	
 	
 	public Enemigo(String nombre, BufferedImage textura, Vector2D posicion) {
-		super(nombre, textura, posicion);
+		super();
+		
 	}
 
 	
